a47034135097a637e4f4b3737c70c2251a20b460
Arvin9 <594113869@qq.com>
2017-11-27 21:29:28 +0800

    1

--- front/views.py
+++ front/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+
+from front.models import Demo
+
 def index(request):
+    d = Demo(name='Beatles Blog')
+    d.save()
     return HttpResponse("Hello, world. You're at the polls index.")
 
